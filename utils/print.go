f81b1eae81f5087a9a3fbba88bfa986fed299d5a
eugene-nn <72197489+eugene-nn@users.noreply.github.com>
2021-11-26 15:56:19 +0200

    Second try to install binary from git repository

---
+++ utils/print.go
@@ -0,0 +1,12 @@
+// declare package utils
+// implement print function in package utils using fmt.println
+// Language: go
+package utils
+
+import "fmt"
+
+
+func Print(s string) {
+	fmt.Println(s)
+}
+
