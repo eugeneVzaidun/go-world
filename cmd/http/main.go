0413a80311179f1c29c6f10ba79693ac8ba10f64
Eugene <72197489+eugene-nn@users.noreply.github.com>
2021-12-04 10:22:35 +0100

    Add get all request

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -3,20 +3,32 @@ package main
 import (
 	"context"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+var client *mongo.Client
 // init function that starts the server
 func init() {
 	// println is a function that prints to the console
 	println("Starting server...")
-	
+	var connectionError error
+	// init connects to the mongodb server
+	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
+	client, connectionError = mongo.Connect(context.TODO(), clientOptions)
+	// print message if connection to mongo is successful
+	println("Connected to MongoDB!")
+
+	if connectionError != nil {
+		log.Fatal(connectionError)
+	}
 }
+
+
 // define user model with json tags
 type User struct {
 	Name string `json:"name"`
@@ -25,17 +37,11 @@ type User struct {
 
 // function that stores user in monogo db and return the user
 func storeUser(user User) User {
-	// store user in mongo db
-	// connect to mongo db
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-	client, err := mongo.Connect(context.TODO(), clientOptions)
-	if err != nil {
-		log.Fatal(err)
-	}
 	// connect to the database
 	db := client.Database("test")
 	// create a collection
 	collection := db.Collection("users")
+	var err error
 	// insert the user
 	_, err = collection.InsertOne(context.TODO(), user)
 	if err != nil {
@@ -71,29 +77,55 @@ func handlePost(w http.ResponseWriter, r *http.Request) {
 	w.Write(json)
 }
 
+// handler that rertieves the user from the database
+func handleGet(w http.ResponseWriter, r *http.Request) {
+	// user logger to log the request
+	println("GET request received")
+	// connect to the database
+	db := client.Database("test")
+	// create a collection
+	collection := db.Collection("users")
+	// get the user from the database
+	var user User
+	collection.FindOne(context.TODO(), bson.M{}).Decode(&user)
+	// write the user to the response
+	json, _ := json.Marshal(user)
+	w.Write(json)
+}
+
+// handler that retrivies all documents from the database
+func handleGetAll(w http.ResponseWriter, r *http.Request) {
+	// user logger to log the request
+	println("GET request received")
+	// connect to the database
+	db := client.Database("test")
+	// create a collection
+	collection := db.Collection("users")
+	// get the user from the database
+	var users []User
+	opts := options.Find().SetSort(bson.D{})
+	cursor, err := collection.Find(context.TODO(), bson.D{}, opts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err = cursor.All(context.TODO(), &users); err != nil {
+		log.Fatal(err)
+	}
+	// write the user to the response
+	json, _ := json.Marshal(users)
+	w.Write(json)
+}
+
 func startServer() {
 	http.HandleFunc("/", handler)
-	http.HandleFunc("/post", handlePost)
+	http.HandleFunc("/user", handlePost)
+	http.HandleFunc("/user", handleGet)
+	http.HandleFunc("/users", handleGetAll)
 	http.ListenAndServe(":8080", nil)
 }
 
 // function that starts the server
 func main() {
-	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
-    client, err := mongo.Connect(context.TODO(), clientOptions)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-
-    err = client.Ping(context.TODO(), nil)
-
-    if err != nil {
-        log.Fatal(err)
-    }
-	fmt.Println("Connected to MongoDB!")
-	
-	// start the server
 	startServer()
 	defer client.Disconnect(context.TODO())
 }
